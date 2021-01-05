f753ad698daa748868406a0a211c2e5bd16b7973
Steven Greve <steven.bryan.greve@gmail.com>
2021-01-05 14:31:15 +0100

    Finished the landing page — plus added all the animations for the navigation bar, the project section, and the footer.

---
+++ script.js
@@ -0,0 +1,93 @@
+// Navbar Animation (for all pages)
+const resume = document.getElementById("resume");
+const about = document.getElementById("about");
+const archive = document.getElementById("archive");
+const contact = document.getElementById("contact");
+const home = document.getElementById("home");
+
+resume.onmouseover = getNavbarEffectResume;
+
+function getNavbarEffectResume(){
+    resume.classList.add("rotate-360");
+
+    setTimeout(function(){
+        resume.classList.remove("rotate-360");
+    }, 2000);
+}
+
+about.onmouseover = getNavbarEffectAbout;
+
+function getNavbarEffectAbout(){
+    about.classList.add("rotate-360");
+
+    setTimeout(function(){
+        about.classList.remove("rotate-360");
+    }, 2000);
+}
+
+archive.onmouseover = getNavbarEffectArchive;
+
+function getNavbarEffectArchive(){
+    archive.classList.add("rotate-360");
+
+    setTimeout(function(){
+        archive.classList.remove("rotate-360");
+    }, 2000);
+}
+
+contact.onmouseover = getNavbarEffectContact;
+
+function getNavbarEffectContact(){
+    contact.classList.add("rotate-360");
+
+    setTimeout(function(){
+        contact.classList.remove("rotate-360");
+    }, 2000);
+}
+
+home.onmouseover = getNavbarEffectHome;
+
+function getNavbarEffectHome(){
+    home.classList.add("rotate-360");
+
+    setTimeout(function(){
+        home.classList.remove("rotate-360");
+    }, 2000);
+}
+
+// Footer Animation (for all pages)
+const youtube = document.getElementById("youtube");
+const github = document.getElementById("github");
+const wordpress = document.getElementById("wordpress");
+const codepen = document.getElementById("codepen");
+const heart = document.getElementById("heart");
+
+youtube.onmouseover = getFooterEffectYoutube;
+
+function getFooterEffectYoutube(){
+    youtube.classList.add("grow");  
+}
+
+github.onmouseover = getFooterEffectGithub;
+
+function getFooterEffectGithub(){
+    github.classList.add("grow");
+}
+
+wordpress.onmouseover = getFooterEffectWordpress;
+
+function getFooterEffectWordpress(){
+    wordpress.classList.add("grow");
+}
+
+codepen.onmouseover = getFooterEffectCodepen;
+
+function getFooterEffectCodepen(){
+    codepen.classList.add("grow");
+}
+
+heart.onmouseover = getFooterEffectHeart;
+
+function getFooterEffectHeart(){
+    heart.classList.add("grow");
+}
