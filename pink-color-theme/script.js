8df6372785eb35784c40f42ea7edce821f85c3dd
Steven Greve <steven.bryan.greve@gmail.com>
2021-01-13 23:48:02 +0100

    Almost finished the landing page.

---
+++ pink-color-theme/script.js
@@ -0,0 +1,104 @@
+// Navigation Bar Animation 
+const resume = document.getElementById("resume");
+const about = document.getElementById("about");
+const archive = document.getElementById("archive");
+const contact = document.getElementById("contact");
+const home = document.getElementById("home");
+const changelog = document.getElementById("changelog");
+
+resume.onmouseover = getNavbarEffectResume;
+
+function getNavbarEffectResume(){
+    resume.classList.add("rotate-360");
+
+    setTimeout(function(){
+        resume.classList.remove("rotate-360");
+    }, 2000);
+}
+
+about.onmouseover = getNavbarEffectAbout;
+
+function getNavbarEffectAbout(){
+    about.classList.add("rotate-360");
+
+    setTimeout(function(){
+        about.classList.remove("rotate-360");
+    }, 2000);
+}
+
+archive.onmouseover = getNavbarEffectArchive;
+
+function getNavbarEffectArchive(){
+    archive.classList.add("rotate-360");
+
+    setTimeout(function(){
+        archive.classList.remove("rotate-360");
+    }, 2000);
+}
+
+contact.onmouseover = getNavbarEffectContact;
+
+function getNavbarEffectContact(){
+    contact.classList.add("rotate-360");
+
+    setTimeout(function(){
+        contact.classList.remove("rotate-360");
+    }, 2000);
+}
+
+changelog.onmouseover = getNavbarEffectChangelog;
+
+function getNavbarEffectChangelog(){
+    changelog.classList.add("rotate-360");
+
+    setTimeout(function(){
+        changelog.classList.remove("rotate-360");
+    }, 2000);
+}
+
+home.onmouseover = getNavbarEffectHome;
+
+function getNavbarEffectHome(){
+    home.classList.add("rotate-360");
+
+    setTimeout(function(){
+        home.classList.remove("rotate-360");
+    }, 2000);
+}
+
+// Footer Animation 
+const youtube = document.getElementById("youtube");
+const github = document.getElementById("github");
+const wordpress = document.getElementById("wordpress");
+const codepen = document.getElementById("codepen");
+const knight = document.getElementById("knight");
+
+youtube.onmouseover = getFooterEffectYoutube;
+
+function getFooterEffectYoutube(){
+    youtube.classList.add("grow");  
+}
+
+github.onmouseover = getFooterEffectGithub;
+
+function getFooterEffectGithub(){
+    github.classList.add("grow");
+}
+
+wordpress.onmouseover = getFooterEffectWordpress;
+
+function getFooterEffectWordpress(){
+    wordpress.classList.add("grow");
+}
+
+codepen.onmouseover = getFooterEffectCodepen;
+
+function getFooterEffectCodepen(){
+    codepen.classList.add("grow");
+}
+
+knight.onmouseover = getFooterEffectKnight;
+
+function getFooterEffectKnight(){
+    knight.classList.add("grow");
+}
